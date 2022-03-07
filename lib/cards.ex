403ee42d9a3b1b2d63a9838a1863dd113856fef3
Heru Pranoto <h3rucutu@gmail.com>
2022-03-07 22:02:38 +0700

    Bug fixed create_deck method to flatten the cascaded list

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,13 +1,16 @@
 defmodule Cards do
+
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for value <- values do
+    cards = for value <- values do
       for suit <- suits do
         "#{value} of #{suit}"
       end
     end
+
+    List.flatten(cards)
   end
 
   def shuffle(deck) do
